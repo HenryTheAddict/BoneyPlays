1ccb2ce4ef349eaac69a1c88630dd4231bfc4eb9
H3nry <1324902498a@gmail.com>
2026-03-19 23:07:29 -0400

    first commit

---
+++ phrasesData.js
@@ -0,0 +1,32 @@
+const phrasesData = {
+  winPhrases: [
+    "{winner} totally mogged {loser}!",
+    "{winner} sent {loser} back to the Jurassic period!",
+    "Flawless victory for {winner}!",
+    "{winner} is the apex predator!",
+    "{loser} couldn't handle {winner}'s MAGIC!",
+    "{winner} swept the floor with {loser}!",
+    "It's a beautiful day for {winner} to win!",
+    "{winner} survived extinction! {loser} did not.",
+    "{winner} just made {loser} look like an absolute clown.",
+    "{loser} should probably just uninstall after what {winner} did to them.",
+    "{winner} didn't even break a sweat destroying {loser}.",
+    "Was {loser} even trying? {winner} dominated.",
+    "{winner} just permanently humbled {loser}."
+  ],
+  deathPhrases: [
+    "{loser} has turned to dust...",
+    "{loser} was absolutely obliterated.",
+    "{loser} is sleeping with the trilobites.",
+    "{loser} suffered a massive skill issue.",
+    "RIP {loser}. They will not be missed.",
+    "{loser} got folded like a lawn chair.",
+    "{loser} failed is with the bones.",
+    "{loser} has logged off permanently.",
+    "{loser} got absolutely stomped into the dirt.",
+    "Embarrassing display by {loser}.",
+    "{loser} got clapped so hard they forgot their own name.",
+    "Did {loser} leave their brain in the Stone Age?",
+    "{loser} is literal garbage at this game."
+  ]
+};
